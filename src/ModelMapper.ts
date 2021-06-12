9f6d20ead350328fb0f5a14b0344951e1013e53d
David Johnston <djohnston@whispir.com>
2021-06-12 17:08:01 +1000

    Add double sine

--- src/ModelMapper.ts
+++ src/ModelMapper.ts
@@ -396,6 +396,209 @@ const preBuiltModels = {
 
       }
     }, ],
+  },
+  "double-sine" : {
+    modelDefinition: [
+
+      {
+        valueType: "number", 
+        valueMaker: "StaticNumberMaker", 
+        params: {
+          value: 0.2
+        }, 
+        id: 'frequency-1', 
+      },
+        
+      {
+        valueType: "number", 
+        valueMaker: "TickingPhaseMaker", 
+        params: {
+          initialValue: Math.PI, 
+          max: 2 * Math.PI, 
+          step: {
+            type: "reference", 
+            reference: "frequency-1",
+          }     
+       }, 
+        id: "phase-1"
+      },
+
+      {
+        valueType: "number", 
+        valueMaker: "Normalizer", 
+        params: {
+          offset: 0, 
+          ratio: 1 / (Math.PI * 2),
+          inputValue: {
+            type: "reference", 
+            reference: "phase-1"
+          }
+        }, 
+        id: "x-1"
+      },
+
+      {
+        valueType: "number", 
+        valueMaker: "SineNumberMaker", 
+        params: {
+          phase: {
+            type: "reference", 
+            reference: "phase-1"
+          }, 
+          amplitude: 0.5
+        },
+        id: "sine-value-1",
+      }, 
+
+      {
+        valueType: "number", 
+        valueMaker: "Normalizer", 
+        params: {
+          offset: 0.5, 
+          ratio: 1,
+          inputValue: {
+            type: "reference", 
+            reference: "sine-value-1"
+          }
+        }, 
+        id: "y-1"
+      },
+
+      {
+        valueType: "position", 
+        valueMaker: "XYPositionMaker", 
+        params: {
+          x: {
+            type: "reference", 
+            reference: "x-1"
+          },
+          y: {
+            type: "reference", 
+            reference: "y-1"
+          },
+        }, 
+        id: "position-1"
+      },
+
+
+      // Number 2
+      {
+        valueType: "number", 
+        valueMaker: "StaticNumberMaker", 
+        params: {
+          value: 0.3
+        }, 
+        id: 'frequency-2', 
+      },
+        
+      {
+        valueType: "number", 
+        valueMaker: "TickingPhaseMaker", 
+        params: {
+          initialValue: 0, 
+          max: 2 * Math.PI, 
+          step: {
+            type: "reference", 
+            reference: "frequency-2",
+          }     
+       }, 
+        id: "phase-2"
+      },
+
+      {
+        valueType: "number", 
+        valueMaker: "Normalizer", 
+        params: {
+          offset: 0, 
+          ratio: 1 / (Math.PI * 2),
+          inputValue: {
+            type: "reference", 
+            reference: "phase-2"
+          }
+        }, 
+        id: "x-2"
+      },
+
+      {
+        valueType: "number", 
+        valueMaker: "SineNumberMaker", 
+        params: {
+          phase: {
+            type: "reference", 
+            reference: "phase-2"
+          }, 
+          amplitude: 0.5
+        },
+        id: "sine-value-2",
+      }, 
+
+      {
+        valueType: "number", 
+        valueMaker: "Normalizer", 
+        params: {
+          offset: 0.5, 
+          ratio: 1,
+          inputValue: {
+            type: "reference", 
+            reference: "sine-value-2"
+          }
+        }, 
+        id: "y-2"
+      },
+
+      {
+        valueType: "position", 
+        valueMaker: "XYPositionMaker", 
+        params: {
+          x: {
+            type: "reference", 
+            reference: "x-2"
+          },
+          y: {
+            type: "reference", 
+            reference: "y-2"
+          },
+        }, 
+        id: "position-2"
+      }
+      
+    ], 
+    drawMakers: [      {
+      drawType: "DrawDot", 
+      params: {
+        p1: {
+          type: "reference", 
+          reference: "position-1", 
+        }, 
+
+      }
+    },
+  
+    {
+      drawType: "DrawDot", 
+      params: {
+        p1: {
+          type: "reference", 
+          reference: "position-2", 
+        }, 
+
+      }
+    }, 
+
+    {
+      drawType: "DrawLinker", 
+      params: {
+        p1: {
+          type: "reference", 
+          reference: "position-1", 
+        }, 
+        p2: {
+          type: "reference", 
+          reference: "position-2", 
+        }, 
+      }
+    }, 
+  ],
   }
 }
 
@@ -403,7 +606,7 @@ const preBuiltModels = {
 export function getRandomModel(): TheWholeModel {
 
 
-  const prebuiltModel = preBuiltModels['simple-sine']; 
+  const prebuiltModel = preBuiltModels['double-sine']; 
   const {modelDefinition, drawMakers} = prebuiltModel; 
 
 
